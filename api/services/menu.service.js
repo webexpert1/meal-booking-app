99dfac19b3ceece7e70ba281d19631251b09f4d7
Richie <richardokoye2@gmail.com>
2019-02-17 02:27:39 +0100

    add menu endpoint

---
+++ api/services/menu.service.js
@@ -0,0 +1,31 @@
+import MealsService from './meal.service';
+import dummyData from '../utils/dummyData';
+
+const { menu } = dummyData;
+
+const MenuService = {
+  fetchMenu() {
+    return menu;
+  },
+  setMenu() {
+    const newMenu = MealsService.fetchAllMeals();
+    newMenu.map(c => menu.push(c));
+    return menu;
+  },
+  addAMeal(id) {
+    const newMeal = MealsService.getAMeal(id);
+    // Check to make sure that the id entered is a valid id of an existing meal
+    if (Object.entries(newMeal).length === 0 || !menu.indexOf(newMeal)) {
+      return {};
+    }
+    // Check if meal exists already
+    const findMeal = menu.some(meal => meal.id === newMeal.id);
+    if (!findMeal) {
+      menu.push(newMeal);
+      return menu;
+    }
+    return newMeal || {};
+  },
+};
+
+export default MenuService;
